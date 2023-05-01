55f65d85f43a0f17aaa92e91f7ba45c67deb92b0
anguzuDaniel <102733205+anguzuDaniel@users.noreply.github.com>
2023-05-01 22:49:41 +0300

    added settings page

--- app/src/main/java/com/danotech/sevaa/UI/fragments/SubscriptionsFragment.java
+++ app/src/main/java/com/danotech/sevaa/UI/fragments/SubscriptionsFragment.java
@@ -28,14 +28,14 @@ public class SubscriptionsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_subcriptions, container, false);
 
-        // using the gradients to change the background color of cards
-        GradientDrawable expenseGradientDrawable = new GradientDrawable(
-                GradientDrawable.Orientation.TL_BR,
-                new int[]{Color.parseColor("#FFCE54"), Color.parseColor("#F68842")});
-
-        // setting the backgrounds
-        CardView expenseCard = view.findViewById(R.id.balance);
-        expenseCard.setBackground(expenseGradientDrawable);
+//        // using the gradients to change the background color of cards
+//        GradientDrawable expenseGradientDrawable = new GradientDrawable(
+//                GradientDrawable.Orientation.TL_BR,
+//                new int[]{Color.parseColor("#FFCE54"), Color.parseColor("#F68842")});
+//
+//        // setting the backgrounds
+//        CardView expenseCard = view.findViewById(R.id.balance);
+//        expenseCard.setBackground(expenseGradientDrawable);
 
         PieChart chart = view.findViewById(R.id.chart);
 
@@ -49,41 +49,42 @@ public class SubscriptionsFragment extends Fragment {
         entries.add(new PieEntry(25f, "Medical"));
 
         PieDataSet dataSet = new PieDataSet(entries, "Expense Categories");
-//        dataSet.setColors(ColorTemplate.PASTEL_COLORS);
+        dataSet.setColors(ColorTemplate.JOYFUL_COLORS);
 //        int[] colors = {Color.parseColor("#8BC34A"), Color.parseColor("#689F38"), Color.parseColor("#558B2F"), Color.parseColor("#33691E")};
 //        dataSet.setColors(colors);
 
-        int[] colors = {Color.parseColor("#63B457"), Color.parseColor("#D45B5B"), Color.parseColor("#F9A74B"), Color.parseColor("#9B59B6"), Color.parseColor("#3498DB"), Color.parseColor("#1ABC9C"), Color.parseColor("#E67E22"), Color.parseColor("#E74C3C"), Color.parseColor("#95A5A6")};
-        dataSet.setColors(colors);
+//        int[] colors = {Color.parseColor("#63B457"), Color.parseColor("#D45B5B"), Color.parseColor("#F9A74B"), Color.parseColor("#9B59B6"), Color.parseColor("#3498DB"), Color.parseColor("#1ABC9C"), Color.parseColor("#E67E22"), Color.parseColor("#E74C3C"), Color.parseColor("#95A5A6")};
+//        dataSet.setColors(colors);
 
         dataSet.setDrawValues(true); // Enable value display
         dataSet.setDrawIcons(true); // Disable icons
         dataSet.setSliceSpace(2f); // Set space between slices
         dataSet.setValueTextColor(Color.TRANSPARENT); // Set label color
-        dataSet.setValueTextSize(12f); // Set label text size
+        dataSet.setValueTextSize(18f); // Set label text size
 
         PieData data = new PieData(dataSet);
         chart.setData(data);
         chart.invalidate();
         chart.setUsePercentValues(true); // Show percentages instead of values
         chart.getDescription().setEnabled(true); // enable chart description
-        chart.getLegend().setEnabled(false); // Disable legend
-        chart.setDrawEntryLabels(false); // Disable drawing labels inside slices
-//        chart.setExtraOffsets(30f, 10f, 30f, 10f); // Add padding around the chart
+        chart.getLegend().setEnabled(true); // Disable legend
+        chart.setDrawEntryLabels(true); // Disable drawing labels inside slices
+//        chart.setExtraOffsets(10f, 10f, 30f, 10f); // Add padding around the chart
         chart.setCenterTextTypeface(Typeface.DEFAULT_BOLD);
         chart.setHoleRadius(60f);
         chart.setTransparentCircleRadius(65f);
         chart.setEntryLabelColor(R.color.color_transaction_figures);
         chart.setEntryLabelTextSize(12f);
+        chart.isUsePercentValuesEnabled();
 
 
-//// Customization options for the pie chart
-//        chart.setDrawEntryLabels(false); // Disable entry labels
-        chart.setDrawHoleEnabled(true); // Enable the hole in the center
-        chart.setHoleRadius(30f); // Set the radius of the hole
-        chart.setHoleColor(Color.TRANSPARENT);
-        chart.setTransparentCircleRadius(40f); // Set the radius of the transparent circle that surrounds the hole
-        chart.setTransparentCircleColor(Color.TRANSPARENT); // Set the color of the transparent circle to transparent
+////// Customization options for the pie chart
+////        chart.setDrawEntryLabels(true); // Disable entry labels
+//        chart.setDrawHoleEnabled(true); // Enable the hole in the center
+//        chart.setHoleRadius(30f); // Set the radius of the hole
+//        chart.setHoleColor(Color.WHITE);
+//        chart.setTransparentCircleRadius(40f); // Set the radius of the transparent circle that surrounds the hole
+//        chart.setTransparentCircleColor(Color.TRANSPARENT); // Set the color of the transparent circle to transparent
 
         // Inflate the layout for this fragment
         return view;
